0b6b9a62ffcac13011737f2092de8a7b2dd4c103
ginsudev <noahlittle199@gmail.com>
2023-05-10 20:17:32 +1000

    Sileo depictions

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@
 # Create JSON for each package
 ./repo/bin/generate-packageinfo.sh
 
+# Create native depictions
+./repo/bin/generate-native-depictions.sh
+
 # Package
 cd repo
 ./bin/packages.sh
--- repo/bin/append-to-control.sh
+++ repo/bin/append-to-control.sh
@@ -14,14 +14,17 @@ PACKAGE_IDENTIFIER=$(grep -i "^Package:" $control_file | cut -d " " -f 2)
 
 DEPICTION_URL="https://ginsu.dev/repo/depictions/index.html?packageId=$PACKAGE_IDENTIFIER"
 ICON_URL="https://ginsu.dev/repo/api/packageinfo/$PACKAGE_IDENTIFIER/icon.png"
+SILEO_DEPICTION_URL="https://ginsu.dev/repo/depictions/native/$PACKAGE_IDENTIFIER.json"
 
-awk -v depiction="$DEPICTION_URL" -v icon="$ICON_URL" '
+awk -v depiction="$DEPICTION_URL" -v icon="$ICON_URL" -v sileoDepiction="$SILEO_DEPICTION_URL" '
     /^Depiction:/ { sub(/.*/, "Depiction: " depiction); found_depiction = 1 }
     /^Icon:/ { sub(/.*/, "Icon: " icon); found_icon = 1 }
+    /^SileoDepiction:/ { sub(/.*/, "SileoDepiction: " sileoDepiction); found_sileoDepiction = 1 }
     { print }
     END { 
       if (!found_depiction) print "Depiction: " depiction 
       if (!found_icon) print "Icon: " icon 
+      if (!found_sileoDepiction) print "SileoDepiction: " sileoDepiction 
     }
 ' "$control_file" > "$control_file.new"
 mv "$control_file.new" "$control_file"
