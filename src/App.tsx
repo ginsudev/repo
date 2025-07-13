6e6cc0f91964238b8be7787a49bc6faf158de4fb
Noah Little <noahlittle199@gmail.com>
2025-07-13 21:55:30 +1000

    attempt fix router

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { BrowserRouter, Routes, Route } from "react-router";
-import HomePage from "./Features/Home/HomePage";
-import PackageDepictionPage from "./Features/Packages/PackageDepictionPage";
-import VersionsPage from "./Features/Packages/VersionsPage";
-import MaxWidthWrapper from "./Components/MaxWidthWrapper";
+import { BrowserRouter, Routes, Route } from 'react-router'
+import HomePage from './Features/Home/HomePage'
+import PackageDepictionPage from './Features/Packages/PackageDepictionPage'
+import VersionsPage from './Features/Packages/VersionsPage'
+import MaxWidthWrapper from './Components/MaxWidthWrapper'
 
 function App() {
   return (
     <MaxWidthWrapper>
-      <BrowserRouter>
+      <BrowserRouter basename={'/'}>
         <Routes>
           <Route index element={<HomePage />} />
-          <Route path="/depiction/:id" element={<PackageDepictionPage />} />
-          <Route path="/versions/:id" element={<VersionsPage />} />
+          <Route path='/depiction/:id' element={<PackageDepictionPage />} />
+          <Route path='/versions/:id' element={<VersionsPage />} />
         </Routes>
       </BrowserRouter>
     </MaxWidthWrapper>
-  );
+  )
 }
 
-export default App;
+export default App
