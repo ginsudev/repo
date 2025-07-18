4db201bfecd8475e9391da25d161d039a87c3f14
Noah Little <noahlittle199@gmail.com>
2025-07-18 20:29:04 +1000

    add pollyfils

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    legacy({
+      polyfills: true,
+      modernPolyfills: true
+    })
+  ]
 })
